1101eb8ba605917521e3231650c72d15bccf4524
RenoirTan <renoirtan2005@gmail.com>
2024-03-11 13:13:33 +0800

    main: move everything

--- tests/experiment_nfa.py
+++ tests/experiment_nfa.py
@@ -1,65 +1,6 @@
 from pprint import pprint
 
-from redfa.nfa import Nfa
-from redfa.transition import NonCharTransition
-
-
-def make_nfa_0():
-    states = {0, 1, 2, 3}
-    starts = {0}
-    transitions = {
-        0: {NonCharTransition.EPSILON: {2, 3}, "b": {1}},
-    }
-    accepts = {1, 3}
-    return Nfa(states, transitions, accepts, starts)
-
-def make_nfa_1():
-    states = set(range(16))
-    transitions = {
-        0: {NonCharTransition.EPSILON: {1, 9}},
-        1: {"a": {2}},
-        2: {NonCharTransition.EPSILON: {3, 5}},
-        3: {"a": {4}},
-        4: {NonCharTransition.EPSILON: {3, 5}},
-        5: {NonCharTransition.EPSILON: {6, 8}},
-        6: {"b": {7}},
-        7: {NonCharTransition.EPSILON: {6, 8}},
-        8: {NonCharTransition.EPSILON: {1, 9}},
-        9: {"a": {10}},
-        10: {NonCharTransition.EPSILON: {11, 13}},
-        11: {"a": {12}},
-        12: {NonCharTransition.EPSILON: {15}},
-        13: {"b": {14}},
-        14: {NonCharTransition.EPSILON: {15}}
-    }
-    accepts = {15}
-    start = {0}
-    return Nfa(states, transitions, accepts, start)
-
-def make_nfa_2():
-    states = {0, 1, 2, 3}
-    starts = {0}
-    transitions = {
-        0: {NonCharTransition.EPSILON: {2}, "a": {1}},
-        1: {"b": {3}},
-        2: {"b": {3}},
-    }
-    accepts = {3}
-    return Nfa(states, transitions, accepts, starts)
-
-def make_nfa_3():
-    # test case from https://www.geeksforgeeks.org/conversion-of-epsilon-nfa-to-nfa/
-    states = {0, 1, 2, 3, 4}
-    starts = {0}
-    transitions = {
-        0: {NonCharTransition.EPSILON: {2}, "1": {1}},
-        1: {"1": {0}},
-        2: {"0": {3}, "1": {4}},
-        3: {"0": {2}},
-        4: {"0": {2}},
-    }
-    accepts = {2}
-    return Nfa(states, transitions, accepts, starts)
+from tests.experiments import make_nfa_3
 
 nfa = make_nfa_3()
 
--- tests/test_nfa.py
+++ tests/test_nfa.py
@@ -1,52 +1,16 @@
 from redfa.nfa import Nfa, find
 from redfa.transition import NonCharTransition
+from tests.experiments import make_nfa_0, make_nfa_1, make_nfa_2
 
 
 # https://cyberzhg.github.io/toolbox/regex2nfa?regex=KGF8YikqYQ==
 def test_nfa_0():
-    states = set(range(9))
-    transitions = {
-        0: {NonCharTransition.EPSILON: {1, 7}},
-        1: {NonCharTransition.EPSILON: {2, 4}},
-        2: {"a": {3}},
-        3: {NonCharTransition.EPSILON: {6}},
-        4: {"b": {5}},
-        5: {NonCharTransition.EPSILON: {6}},
-        6: {NonCharTransition.EPSILON: {1, 7}},
-        7: {"a": {8}}
-    }
-    accepts = {8}
-    start = {0}
-    nfa = Nfa(states, transitions, accepts, start)
+    nfa = make_nfa_0()
     
     assert find(nfa, "a") == (0, 1)
     assert find(nfa, "b") is None
     assert find(nfa, "aa") == (0, 2)
     assert find(nfa, "ca") == (1, 2)
-
-
-def make_nfa_1():
-    states = set(range(16))
-    transitions = {
-        0: {NonCharTransition.EPSILON: {1, 9}},
-        1: {"a": {2}},
-        2: {NonCharTransition.EPSILON: {3, 5}},
-        3: {"a": {4}},
-        4: {NonCharTransition.EPSILON: {3, 5}},
-        5: {NonCharTransition.EPSILON: {6, 8}},
-        6: {"b": {7}},
-        7: {NonCharTransition.EPSILON: {6, 8}},
-        8: {NonCharTransition.EPSILON: {1, 9}},
-        9: {"a": {10}},
-        10: {NonCharTransition.EPSILON: {11, 13}},
-        11: {"a": {12}},
-        12: {NonCharTransition.EPSILON: {15}},
-        13: {"b": {14}},
-        14: {NonCharTransition.EPSILON: {15}}
-    }
-    accepts = {15}
-    start = {0}
-    return Nfa(states, transitions, accepts, start)
 
 
 # https://cyberzhg.github.io/toolbox/nfa2dfa?regex=KGErYiopKmEoYXxiKQ==
@@ -67,22 +31,6 @@ def test_nfa_1_no_epsilon():
     assert find(nfa, "c") is None
     assert find(nfa, "baab") == (1, 4)
     assert find(nfa, "acb") is None
-
-
-def make_nfa_2():
-    # (11)*(00|10)*
-    # test case from https://www.geeksforgeeks.org/conversion-of-epsilon-nfa-to-nfa/
-    states = {0, 1, 2, 3, 4}
-    starts = {0}
-    transitions = {
-        0: {NonCharTransition.EPSILON: {2}, "1": {1}},
-        1: {"1": {0}},
-        2: {"0": {3}, "1": {4}},
-        3: {"0": {2}},
-        4: {"0": {2}},
-    }
-    accepts = {2}
-    return Nfa(states, transitions, accepts, starts)
 
 
 def test_nfa_2():
