da882a1a14a6968e0da87096e709a10730d4ef78
RenoirTan <renoirtan2005@gmail.com>
2025-01-02 15:37:52 +0800

    groups: add tests, test_nfagroup_3 fails

---
+++ tests/test_nfagroup.py
@@ -0,0 +1,43 @@
+from redfa.thompson import thompson
+from redfa import nfa
+
+
+def get_groups_from_match(m: nfa.NfaMatch):
+    groups = []
+    for group_arr in m.groups_.values():
+        groups.extend(group_arr)
+    return sorted([m.string_[b:e] for b, e in groups])
+
+
+def test_nfagroup_0():
+    r = thompson(r"(aa)*aab")
+    assert r is not None
+    m = nfa.match(r, "aaaab")
+    assert m is not None
+    assert get_groups_from_match(m) == ["aa"]
+
+
+def test_nfagroup_1():
+    r = thompson(r"(a+b*)*a(a|b)")
+    assert r is not None
+    m = nfa.match(r, "aaaab")
+    assert m is not None
+    assert get_groups_from_match(m) == ["a", "a", "a", "b"]
+
+
+def test_nfagroup_2():
+    r = thompson(r"(ab(cd)*ef)+")
+    assert r is not None
+    m = nfa.match(r, "abcdefabefabcdcdef")
+    assert m is not None
+    assert get_groups_from_match(m) == ["abcdcdef", "abcdef", "abef", "cd", "cd", "cd"]
+
+
+def test_nfagroup_3():
+    r = thompson(r"(ab((cd)*)ef)+")
+    assert r is not None
+    m = nfa.match(r, "abcdefabefabcdcdef")
+    assert m is not None
+    assert get_groups_from_match(m) == [
+        "abcdcdef", "abcdef", "abef", "cd", "cd", "cd", "cd", "cdcd"
+    ]
